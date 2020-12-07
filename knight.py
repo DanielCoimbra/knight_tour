42f38ce42779e24ac7efa3c97e8ba9ac48f6cbd2
Daniel <coimbrasantos.d@gmail>
2020-12-07 01:20:16 -0300

    more changes

--- knight.py
+++ knight.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 class Knight:
     MOVES=[
         [1,2],
@@ -9,31 +11,38 @@ class Knight:
         [-2, 1],
         [-2, -1]
         ]
+    All_knights=[]
+    Winners=[]
 
 # -------------------------------- Create Knight ------------------------------------
-    def __init__(self, place):
+    def __init__(self, place=None, ghost=False):
+        print(self)
         self.board=[]
         self.moves=[]
-        self.place(place)
         self.create_board()
+        self.ghost = ghost
+        
+
+        if ghost:
+            for move in self.moves:
+                self.place(move)
+
+        else:
+            self.place(place)
+
+        Knight.All_knights.append(self)
 
 # -------------------------------- Place Knight ------------------------------------
     def place(self,place):
         self.moves.append(place)
+        self.board.remove(place)
+        print("knight to : "+str(place))
 
-# -------------------------------- Move Knight ------------------------------------
-    def move(self, move_pair):
-        self.moves.append(move_pair)
-        last_move = self.moves[-1]
-        self.mark(last_move)
 
 # -------------------------------- Mark used squares ------------------------------------
-    def mark(self, square):
-        self.board.remove(square)
+#     def mark(self, square):
+#         self.board.remove(square)
 
-
-    def check_move(self):
-        self.
 
 # -------------------------------- create board ------------------------------------
     def create_board(self):
@@ -52,8 +61,37 @@ class Knight:
             if new_pos in self.board:
                 squares.append(new_pos)
 
-# -------------------------------- Replicate ghosts knights ------------------------------------
-    def fragment_knight(self,knight):
+        return squares
 
-        new_knight = Knight(knight.moves[-1])
-        new_knight.moves = knight.moves
+# -------------------------------- Replicate ghosts knights ------------------------------------
+    def fragment_knight(self):
+        var = 0
+        for move in self.possible_squares():
+            new_knight=None
+            print("\nmove: "+ str(var+1))
+            print(move)
+            sleep(.5)
+            new_knight = Knight(ghost=True)
+            
+            print(new_knight.board)
+            new_knight.moves = self.moves
+            new_knight.place(move)
+            print("\nknights: "+str(len(Knight.All_knights)))
+            var += 1
+
+        print('\nThis knight is dead: ' + str(self))
+        del self
+        print("\nknights: "+ str(len(Knight.All_knights)))
+
+    def print(self):
+        for move in self.moves:
+            print(str(move) + '\n')
+def tour():
+    k = Knight(place=[1,1])
+    k.place([k.possible_squares()[0]])
+
+    for x in range(8):
+        for y in range(8):
+            k = Knight([x+1, y+1])
+            print(k.moves[0])
+            print(k.possible_squares())
